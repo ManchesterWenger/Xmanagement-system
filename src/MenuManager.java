444c4e19558e5a598ab87f0e28b483fb26b3cf1d
ManchesterWenger <gkstmddn12@naver.com>
2022-04-27 16:01:40 +0900

    test

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,82 +1,40 @@
 import java.util.Scanner;
 public class MenuManager {
-	
-	public static void addPlayer() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Player Number:");
-		int playerNumber = input.nextInt();
-		System.out.print("Player Name:");
-		String playerName = input.next();
-		System.out.print("Player age:");
-		int playerAge = input.nextInt();
-	}
-	
-	public static void Deleteplayer(){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Player Number:");
-		int playerNumber = input.nextInt();
-		System.out.print("Player Name:");
-		String playerName = input.next();
-		System.out.print("Player age:");
-		int playerAge = input.nextInt();
-		
-	}
-	
-	public static void Editplayer() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Player Number:");
-		int playerNumber = input.nextInt();
-		System.out.print("Player Name:");
-		String playerName = input.next();
-		System.out.print("Player age:");
-		int playerAge = input.nextInt();
-	}
-	
-	public static void Viewplayer() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Player Number:");
-		int playerNumber = input.nextInt();
-		System.out.print("Player Name:");
-		String playerName = input.next();
-		System.out.print("Player age:");
-		int playerAge = input.nextInt();
-	}
-	
+
 	
 
 	public static void main(String[] args) {
-
-		int num = 0;
+		
 		Scanner input = new Scanner(System.in);
-		while(num!=6) {
+		PlayerManger playerManger = new PlayerManger(input);
+		int num = -1;
+		
+		while(num!=5) {
 
 		System.out.println("1. Add player");
 		System.out.println("2. Delete player");
 		System.out.println("3. Edit player");
 		System.out.println("4. View player");
-		System.out.println("5. Show a menu");
-		System.out.println("6. Exit");
-		System.out.print("Select one number between 1 - 6:");
+		System.out.println("5. Exit");
+		System.out.print("Select one number between 1 - 5:");
 		num = input.nextInt();
 		if (num==1) {
-			 addPlayer();
+			playerManger.addPlayer();
 					}
 		else if (num==2) {
 			
-			Deleteplayer();
+			playerManger.Deleteplayer();
 					}
 		else if (num==3) {
-			Editplayer();
+			playerManger.Editplayer();
 					}
 		else if (num==4) {
-			Viewplayer();
+			playerManger.Viewplayer();
 					}
+		
 		else if (num==5) {
-			continue;
-					}
-		else if (num==6) {
 			System.out.print("management System over");
-			
+			break;
 					}
 		
 		
