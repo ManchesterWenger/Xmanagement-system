af2e1a33f19bfceb3a4c2d7fefa65cbfe78b7f0e
82103 <82103@DESKTOP-G1FJB1K>
2022-05-02 18:56:30 +0900

    edit sector

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -14,7 +14,7 @@ public class MenuManager {
 		System.out.println("1. Add player");
 		System.out.println("2. Delete player");
 		System.out.println("3. Edit player");
-		System.out.println("4. View player");
+		System.out.println("4. View players");
 		System.out.println("5. Exit");
 		System.out.print("Select one number between 1 - 5:");
 		num = input.nextInt();
@@ -29,7 +29,7 @@ public class MenuManager {
 			playerManger.Editplayer();
 					}
 		else if (num==4) {
-			playerManger.Viewplayer();
+			playerManger.Viewplayers();
 					}
 		
 		else if (num==5) {
--- src/PlayerManger.java
+++ src/PlayerManger.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PlayerManger {
-	Player player;
+	ArrayList<Player> players= new ArrayList<Player>();
 	Scanner input;
 	
 	PlayerManger(Scanner input){
 		this.input=input;
 		}
 	public  void addPlayer() {
-		player = new Player();
+		Player player = new Player();
 		System.out.print("Player Number:");
 		player.number = input.nextInt();
 		System.out.print("Player Name:");
@@ -17,37 +18,80 @@ public class PlayerManger {
 		player.age = input.nextInt();
 		System.out.print("Player wage:");
 		player.wage = input.nextLong();
+		players.add(player);
 	}
 	
 	public  void Deleteplayer(){
 		System.out.print("Player Name:");
 		String playerName = input.next();
-		if(player==null) {
+		int index= -1;
+		for(int i=0; i<players.size(); i++) {
+			if(players.get(i).name== playerName) {
+				index=i;
+				break;			
+			}
+		}
+		if(index>=0) {
+			players.remove(index);
+			System.out.println("the player"+ playerName+ "is deleted");
+		}
+		else {
 			System.out.println("the player has not been registered");
 			return;
 		}
-		if(player.name== playerName) {
-			player=null;
-			System.out.println("the player is deleted");
-		}
-		
+				
 	}
 	
 	public  void Editplayer() {
-		
-		
 		System.out.print("Player Name:");
 		String playerName = input.next();
-		if(player.name== playerName) {			
-			System.out.println("the player to be edited is"+playerName);
-		}
+		for(int i=0; i<players.size(); i++) {
+		Player player = players.get(i);
+		if(player.name.equals(playerName)) {			
+			int num = -1;
+			while (num!= 5) {
+				System.out.println("** Player Edit Menu");
+				System.out.println("1. Edit Name");
+				System.out.println("2. Edit Number");
+				System.out.println("3. Edit age");
+				System.out.println("4. Edit wage");
+				System.out.println("5. Exit");
+				System.out.println("Select one number between 1 - 5");
+				Scanner input = new Scanner(System.in);
+				num=input.nextInt();
+				if(num ==1) {
+					System.out.print("Player Name:");
+					player.name=input.next();
+				}
+				else if (num==2) {
+					System.out.print("Player Number:");
+					player.number=input.nextInt();
+				}
+				else if (num==3) {
+					System.out.print("Player age:");
+					player.age=input.nextInt();
+				}
+				else if (num==4) {
+					System.out.print("Player wage:");
+					player.wage=input.nextLong();
+				}
+				else {
+					continue;
+				}
+			}
+		break;	
+		  }		
+		} 		
 	}
 	
-	public  void Viewplayer() {	
-		System.out.print("Player Name:");
-		String playerName = input.next();
-		if(player.name.equals(playerName)) {			
-			player.printInfo();
+	public  void Viewplayers() {	
+//		System.out.print("Player Name:");
+//		String playerName = input.next();
+		for(int i=0; i<players.size(); i++) {
+			players.get(i).printInfo();
 		}
+//		if(player.name.equals(playerName)) {			
+//			player.printInfo();
+//		}
 	}
 }
