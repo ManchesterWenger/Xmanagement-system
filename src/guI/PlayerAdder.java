b0461c5a975a860f69234feddade16b93752aae9
82103 <82103@DESKTOP-G1FJB1K>
2022-06-07 11:06:35 +0900

    edit section

--- src/guI/PlayerAdder.java
+++ src/guI/PlayerAdder.java
@@ -1,5 +1,27 @@
 package guI;
 
-public class PlayerAdder {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class PlayerAdder extends JFrame {
+	
+	public PlayerAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout (new SpringLayout());
+		
+		JLabel labelName = new JLabel ("Name:", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		SpringUtilities.makeCompactGrid(panel, 1, 2, 6, 6, 6, 6);
+		
+		this.setSize(300,300);
+		this.setContentPane(panel);
+		this.setVisible(true);
+	}
 
 }
