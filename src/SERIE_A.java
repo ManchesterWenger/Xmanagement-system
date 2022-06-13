9a44176a562789fe874104d86884877252bbbc6b
82103 <82103@DESKTOP-G1FJB1K>
2022-06-13 21:36:27 +0900

    1

---
+++ src/SERIE_A.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SERIE_A extends Player implements PlayerInput {
+
+	public void getUserInput(Scanner input) {
+		System.out.print("Player Number:");
+		int number = input.nextInt();
+		this.setNumber(number);
+		
+		System.out.print("Player Name:");
+		String name = input.next();
+		this.setName(name);
+		
+//		System.out.print("Player League:");
+//		String League = input.next();
+//		this.setleague(League);
+		System.out.print("Player age:");
+		int age = input.nextInt();
+		this.setAge(age);
+		
+		System.out.print("Player wage:");
+		long wage = input.nextLong();
+		this.setWage(wage);		
+		
+	}
+
+}
